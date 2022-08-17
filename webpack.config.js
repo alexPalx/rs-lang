2e7904bc7c3b11afd78655bfecf72c3ecdfb1316
Alex Palx <alex.teanow@gmail.com>
2022-08-18 01:10:35 +0300

    init: project initialization

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+  mode = 'production';
+}
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    filename: '[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  mode,
+  devServer: {
+    static: './dist',
+    port: 3000,
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'rs-lang',
+      filename: 'index.html',
+      template: path.resolve(__dirname, './src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'src/assets/svg'),
+        to: path.resolve(__dirname, 'dist/assets/svg'),
+        noErrorOnMissing: true
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/img'),
+        to: path.resolve(__dirname, 'dist/assets/img'),
+        noErrorOnMissing: true
+      }],
+    }),
+    new ESLintPlugin({
+      extensions: 'ts',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /(\.css|\.s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: 'svg-inline-loader'
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource',
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  experiments: {
+    topLevelAwait: true,
+  },
+};
