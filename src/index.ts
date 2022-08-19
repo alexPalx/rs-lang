3f3be59e842d82294478fc250aa1af24ab9d390a
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-08-19 21:20:45 +0300

    refactor: delete committed code

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
 import './sass/main.scss';
-
-// console.log(API.getWords().then((data) => console.log(data)));
-
-// API.loginUser({ email: 'doka@gmail.com', password: '3324522567' }).then((data) => {
-//   console.log(data);
-// });
