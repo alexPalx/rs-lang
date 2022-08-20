79073584edbfd8608518ebaac915ab7584cead24
Alex Palx <alex.teanow@gmail.com>
2022-08-20 06:08:37 +0300

    refactor: hamburger refactoring

--- src/components/view/view.ts
+++ src/components/view/view.ts
@@ -1,9 +1,22 @@
 import Content from './content';
+import { closeNav, openNav } from '../sidenav';
 
 export default class View {
   public content: Content;
 
+  public hamburger: HTMLElement;
+
   constructor() {
     this.content = new Content();
+    this.hamburger = <HTMLElement>document.getElementById('hamburger');
+
+    this.hamburger.addEventListener('click', () => {
+      if (this.hamburger.classList.contains('is-active')) {
+        closeNav();
+      } else {
+        openNav();
+      }
+      this.hamburger.classList.toggle('is-active');
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 import './sass/main.scss';
 import App from './components/app/app';
-import { closeNav, openNav } from './components/sidenav';
-
-const hamburger = document.getElementById('hamburger');
-
-hamburger?.addEventListener('click', () => {
-  if (hamburger.classList.contains('is-active')) {
-    closeNav();
-  } else {
-    openNav();
-  }
-  hamburger.classList.toggle('is-active');
-});
 
 window.addEventListener('DOMContentLoaded', () => {
   const app = new App();
