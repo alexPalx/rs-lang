efaebd1730cf9608f149403e4ab1f5c8536b9501
Alex Palx <alex.teanow@gmail.com>
2022-08-25 23:32:16 +0300

    feat: implement about page, improve auth page

--- src/pages/aboutPage.ts
+++ src/pages/aboutPage.ts
@@ -1,14 +1,41 @@
-import { QueryParam } from '../interfaces/types';
+import { DevInfo } from '../interfaces/types';
 import Component from '../common/component';
+import Developer from './components/developer';
 
 export default class AboutPage extends Component {
-  public wrapper: Component;
+  public title: Component;
+  public devList: Component<HTMLElement>;
 
-  public content: Component;
+  constructor(parentElement: HTMLElement) {
+    super(parentElement, 'div', 'about-wrapper');
+    this.title = new Component(this.node, 'h2', 'about-title', 'О команде');
 
-  constructor(parentElement: HTMLElement, params: QueryParam[] | null) {
-    super(parentElement);
-    this.wrapper = new Component(this.node);
-    this.content = new Component(this.wrapper.node, 'p', '', `AboutPage ${JSON.stringify(params)}`);
+    this.devList = new Component(this.node, 'ul', 'about-list');
+
+    const devInfoList: DevInfo[] = [
+      {
+        imageSrc: '*devImage*',
+        name: 'Алексей Палюхович',
+        github: 'https://github.com/alexPalx',
+        position: 'Разработчик',
+        completedTasks: ['Таск 1', 'таск 2', 'таск 3', 'таск 4'],
+      },
+      {
+        imageSrc: '*devImage*',
+        name: 'Виталий Дреко',
+        github: 'https://github.com/dokahp',
+        position: 'Разработчик',
+        completedTasks: ['Таск 1', 'таск 2', 'таск 3', 'таск 4'],
+      },
+      {
+        imageSrc: '*devImage*',
+        name: 'Сергей Черняк',
+        github: 'https://github.com/atcherdsd',
+        position: 'Разработчик',
+        completedTasks: ['Таск 1', 'таск 2', 'таск 3', 'таск 4'],
+      },
+    ];
+
+    devInfoList.forEach((dev: DevInfo) => new Developer(this.devList.node, dev));
   }
 }
