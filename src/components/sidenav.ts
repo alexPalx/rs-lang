c2a9461e706ec7ba770f184d1a2ec087a61a8350
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-09-02 08:26:32 +0300

    feat: add sidenav styling and adaptive

--- src/components/sidenav.ts
+++ src/components/sidenav.ts
@@ -1,8 +1,14 @@
 export const openNav = () => {
-  (<HTMLElement>document.getElementById('sidenav')).style.width = '250px';
-  (<HTMLElement>document.getElementById('main')).style.marginLeft = '250px';
-  (<HTMLElement>document.querySelector('.header')).style.marginLeft = '250px';
-  (<HTMLElement>document.querySelector('.footer')).style.marginLeft = '250px';
+  let width = 250;
+  if (window.screen.width < 475) {
+    width = window.screen.width;
+    (<HTMLElement>document.getElementById('sidenav')).style.width = `${width}px`;
+  } else {
+    (<HTMLElement>document.getElementById('sidenav')).style.width = `${width}px`;
+    (<HTMLElement>document.getElementById('main')).style.marginLeft = `${width}px`;
+    (<HTMLElement>document.querySelector('.header')).style.marginLeft = `${width}px`;
+    (<HTMLElement>document.querySelector('.footer')).style.marginLeft = `${width}px`;
+  }
 };
 
 export const closeNav = () => {
