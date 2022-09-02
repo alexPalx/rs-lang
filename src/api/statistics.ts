8309a35ba609aa3c1b3168044e9e3cc964fadec7
Alex Palx <alex.teanow@gmail.com>
2022-09-02 17:14:13 +0300

    feat: implement statistics saving

--- src/api/statistics.ts
+++ src/api/statistics.ts
@@ -2,7 +2,7 @@ import { RequestMethod, StatisticsData } from '../interfaces/typesAPI';
 import Utils from '../utils/utils';
 import API from './api';
 
-export default class Statistics {
+export default class UserStatistics {
   public static async get(userId: string): Promise<StatisticsData | undefined> {
     const responseData = await API.sendRequest<StatisticsData>(
       Utils.buildLink(['users', userId, 'statistics']),
@@ -10,17 +10,51 @@ export default class Statistics {
       undefined,
       true
     );
-    return responseData;
+    const returnData = responseData
+      ? {
+          learnedWords: responseData.learnedWords,
+          optional: {
+            startDate: responseData.optional.startDate,
+            sprintStreakPerDay: JSON.parse(
+              <string>(<unknown>responseData.optional.sprintStreakPerDay)
+            ),
+            audioStreakPerDay: JSON.parse(
+              <string>(<unknown>responseData.optional.audioStreakPerDay)
+            ),
+            newWordsPerDay: JSON.parse(<string>(<unknown>responseData.optional.newWordsPerDay)),
+            learnedWordsPerDay: JSON.parse(
+              <string>(<unknown>responseData.optional.learnedWordsPerDay)
+            ),
+            learnedWordsList: JSON.parse(<string>(<unknown>responseData.optional.learnedWordsList)),
+            seenWordsList: JSON.parse(<string>(<unknown>responseData.optional.seenWordsList)),
+          },
+        }
+      : undefined;
+
+    return returnData;
   }
 
   public static async update(
     userId: string,
     statisticsData: StatisticsData
   ): Promise<StatisticsData | undefined> {
+    console.warn(statisticsData.optional.newWordsPerDay);
+    console.warn(JSON.stringify(statisticsData.optional.newWordsPerDay));
     const responseData = await API.sendRequest<StatisticsData>(
       Utils.buildLink(['users', userId, 'statistics']),
       RequestMethod.PUT,
-      statisticsData,
+      {
+        learnedWords: statisticsData.learnedWords,
+        optional: {
+          startDate: statisticsData.optional.startDate,
+          sprintStreakPerDay: JSON.stringify(statisticsData.optional.sprintStreakPerDay),
+          audioStreakPerDay: JSON.stringify(statisticsData.optional.audioStreakPerDay),
+          newWordsPerDay: JSON.stringify(statisticsData.optional.newWordsPerDay),
+          learnedWordsPerDay: JSON.stringify(statisticsData.optional.learnedWordsPerDay),
+          learnedWordsList: JSON.stringify(statisticsData.optional.learnedWordsList),
+          seenWordsList: JSON.stringify(statisticsData.optional.seenWordsList),
+        },
+      },
       true
     );
     return responseData;
